e619fb48f61b3e4ef01f2e6ab3da29081880b9c9
tianweipeng <767339439@qq.com>
2024-06-01 01:46:40 +0800

    添加讲师头像

--- src/views/edu/teacher/save.vue
+++ src/views/edu/teacher/save.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="app-container">
     <el-form ref="form" :model="teacher" label-width="80px" >
+    <el-form-item label="讲师头像">
+      <!-- 头衔缩略图 -->
+      <pan-thumb :image="teacher.avatar"/>
+      <!-- 文件上传按钮 -->
+      <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+      </el-button>
+
+      <!--
+      v-show：是否显示上传组件
+      :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+      :url：后台上传的url地址
+      @close：关闭上传组件
+      @crop-upload-success：上传成功后的回调 
+      <input type="file" name="file"/>
+      -->
+      <image-cropper
+                    v-show="imagecropperShow"
+                    :width="300"
+                    :height="300"
+                    :key="imagecropperKey"
+                    :url="BASE_API+'/eduoss/fileoss'"
+                    field="file"
+                    @close="close"
+                    @crop-upload-success="cropSuccess"/>
+      </el-form-item>
       <el-form-item label="讲师姓名">
         <el-input v-model="teacher.name"/>
       </el-form-item>
@@ -16,18 +41,23 @@
       <el-form-item label="讲师生涯">
         <el-input v-model="teacher.career"/>
       </el-form-item>
-      <el-form-item size="large">
-        <el-button type="primary" @click="saveOrUpdate">确认</el-button>
-      </el-form-item>
+        <el-form-item>
+        <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
+        </el-form-item>
     </el-form>
   </div>
 </template>
 
+
 <script>
 
 import teacherApi from '@/api/teacher/teacher'
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
+
 
 export default {
+  components: { ImageCropper, PanThumb },
   data() {
     return { // 定义变量和初始值
       teacher: {
@@ -37,10 +67,13 @@ export default {
         career: '',
         avatar: ''
       }, // 条件封装对象
-      saveBtnDisabled: false // 保存按钮是否禁用
+      saveBtnDisabled: false, // 保存按钮是否禁用
+      imagecropperShow: false,
+      imagecropperKey: 0,
+      BASE_API: process.env.BASE_API,
     }
   },
-  watch: { // 监听路由发生变化
+  watch: { // 监听路由发生变化，解决点击修改讲师后，再点击添加讲师，回显数据依然存在的问题
     $route(to, from) {
       this.init()
     }
@@ -97,6 +130,14 @@ export default {
           this.teacher = response.data.items
           console.log(this.teacher)
         })
+    },
+    close() {
+      this.imagecropperShow=false
+      this.imagecropperKey = this.imagecropperKey+1 //初始化头像上传组件，否则需要更换图片时，还是上一次上传成功或失败的额状态
+    },
+    cropSuccess(data) {
+      this.teacher.avatar = data.url
+      this.close();
     }
   }
 }
